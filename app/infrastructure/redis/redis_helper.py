7c62e31938f7c44c8de8c26e5e952efd708e3578
Maksim Volosh <maksgoza9@gmail.com>
2025-07-24 01:18:17 +0300

    feat: add redis helper

---
+++ app/infrastructure/redis/redis_helper.py
@@ -0,0 +1,17 @@
+import redis.asyncio as redis
+from app.config import settings 
+
+class RedisHelper:
+    def __init__(self, url: str) -> None:
+        self.client = redis.from_url(url)
+
+    async def dispose(self) -> None:
+        await self.client.close()
+
+    async def get_client(self):
+        yield self.client
+
+
+redis_helper = RedisHelper(
+    url=str(settings.cache.url)
+)
