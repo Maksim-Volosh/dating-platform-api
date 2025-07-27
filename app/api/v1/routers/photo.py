067f58c47090e8c0ab9898bb4ba575497b06d964
Maksim Volosh <maksgoza9@gmail.com>
2025-07-27 12:33:41 +0300

    refactor: refactor

--- app/api/v1/routers/photo.py
+++ app/api/v1/routers/photo.py
@@ -105,38 +105,3 @@ async def update_user_photos(
         user_id=telegram_id,
         photos=[PhotoURL(url=p.url) for p in photo_urls]
     )
-    # user = await db.get(User, user_id)
-    # if user is None:
-    #     raise HTTPException(
-    #         status_code=404, detail=f"User with Id: {user_id} not found"
-    #         )
-    
-    # q = select(Photo).where(Photo.user_id == user_id)
-    # result = await db.execute(q)
-    # photos_models = result.scalars().all()
-
-    # for photo in photos_models:
-    #     await db.delete(photo)
-        
-    # photo_objs = []
-    # for file in photos:
-        
-    #     file_ext = os.path.splitext(f"{file.filename}")[1].lower()
-    #     if file_ext not in [".jpg", ".jpeg", ".png", ".webp"]:
-    #         raise HTTPException(
-    #             400, 
-    #             f"You can upload only .jpg, .jpeg, .png or .webp files but not {file_ext}"
-    #         )
-        
-    #     contents = await file.read()
-    #     unique_name = f"{uuid.uuid4()}_{file.filename}"
-    #     save_path = os.path.join(settings.static.directory, unique_name)
-
-    #     async with aiofiles.open(save_path, "wb") as f:
-    #         await f.write(contents)
-
-    #     new_photo = Photo(url=f"{settings.static.url}/{unique_name}", user_id=user_id)
-    #     db.add(new_photo)
-    #     photo_objs.append(new_photo)
-
-    # await db.commit()
