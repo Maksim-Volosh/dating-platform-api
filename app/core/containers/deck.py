406dc4efff38a2b45a3b45be8cca9b157791cf0c
Maksim Volosh <maksgoza9@gmail.com>
2025-07-28 19:52:00 +0300

    feat: add container for UserDeckUseCase

---
+++ app/core/containers/deck.py
@@ -0,0 +1,19 @@
+from fastapi import Depends
+from app.domain.use_cases import UserDeckUseCase
+from app.infrastructure.db import db_helper
+from app.infrastructure.redis import redis_helper
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.infrastructure.repositories import DeckRedisCache
+from app.infrastructure.repositories import SQLAlchemyUserRepository
+from redis import Redis
+
+async def get_user_deck_use_case(
+    db: AsyncSession = Depends(db_helper.session_getter),
+    client: Redis = Depends(redis_helper.get_client)
+) -> UserDeckUseCase:
+    user_repo = SQLAlchemyUserRepository(db)
+    cache = DeckRedisCache(client)
+    return UserDeckUseCase(
+        user_repo=user_repo,
+        cache=cache
+    )
