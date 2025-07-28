691f4ae9c663f44e2a8ea9a05daef3edcdd14454
Maksim Volosh <maksgoza9@gmail.com>
2025-07-28 19:43:20 +0300

    feat: add to_dict method to UserEntity

--- app/domain/entities/user.py
+++ app/domain/entities/user.py
@@ -10,3 +10,14 @@ class UserEntity:
     gender: str
     prefer_gender: str
     description: Optional[str] = None
+    
+    def to_dict(self):
+        return {
+            "telegram_id": self.telegram_id,
+            "name": self.name,
+            "age": self.age,
+            "city": self.city,
+            "gender": self.gender,
+            "prefer_gender": self.prefer_gender,
+            "description": self.description,
+        }
