b54a6b4e2b19dd18052b6c40b31a20a56306d84e
Maksim Volosh <maksgoza9@gmail.com>
2025-07-30 12:05:27 +0300

    chore: swap fields in the model Swipe

--- app/infrastructure/models/swipe.py
+++ app/infrastructure/models/swipe.py
@@ -9,6 +9,6 @@ class Swipe(Base):
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
     user1_id: Mapped[int]
-    user2_id: Mapped[int]
     user1_decision: Mapped[Optional[bool]] = mapped_column(nullable=True)
+    user2_id: Mapped[int]
     user2_decision: Mapped[Optional[bool]] = mapped_column(nullable=True)
