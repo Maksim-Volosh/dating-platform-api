fd8891c7e215ef7698a720712b267a1628620c0c
Maksim Volosh <maksgoza9@gmail.com>
2025-07-30 12:09:56 +0300

    feat: create swipe container

---
+++ app/core/containers/swipe.py
@@ -0,0 +1,18 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.domain.use_cases import SwipeUserUseCase
+from app.infrastructure.db import db_helper
+from app.infrastructure.repositories import SQLAlchemyUserRepository
+from app.infrastructure.repositories.swipe import SQLAlchemySwipeRepository
+
+
+async def get_swipe_user_use_case(
+    db: AsyncSession = Depends(db_helper.session_getter)
+) -> SwipeUserUseCase:
+    swipe_repo = SQLAlchemySwipeRepository(db)
+    user_repo = SQLAlchemyUserRepository(db)
+    return SwipeUserUseCase(
+        swipe_repo=swipe_repo,
+        user_repo=user_repo
+    )
