92b36deee8e427d9567c15413a195c787a22a552
Maksim Volosh <maksgoza9@gmail.com>
2025-11-30 15:43:38 +0200

    feat: delete photo_storage interface, change IPhotoRepository

--- app/domain/interfaces/__init__.py
+++ app/domain/interfaces/__init__.py
@@ -1,7 +1,6 @@
 __all__ = (
     "IUserRepository",
     "IPhotoRepository",
-    "IPhotoStorage",
     "IDeckCache",
     "ISwipeRepository",
     "ICandidateRepository",
@@ -9,7 +8,6 @@ __all__ = (
 )
 from .deck_cache import IDeckCache
 from .photo import IPhotoRepository
-from .photo_storage import IPhotoStorage
 from .user import IUserRepository
 from .swipe import ISwipeRepository
 from .candidate import ICandidateRepository
--- app/domain/interfaces/photo.py
+++ app/domain/interfaces/photo.py
@@ -1,20 +1,20 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from app.domain.entities.photo import PhotoUniqueNameEntity, PhotoUrlEntity
+from app.domain.entities.photo import PhotoEntity
 
 
 class IPhotoRepository(ABC):
     @abstractmethod
-    async def get_by_user_id(self, telegram_id: int) -> List[PhotoUrlEntity] | None:
+    async def get_by_user_id(self, telegram_id: int) -> List[PhotoEntity] | None:
         raise NotImplementedError
 
     @abstractmethod
     async def create(
-        self, telegram_id: int, unique_names: List[PhotoUniqueNameEntity]
-    ) -> List[PhotoUrlEntity]:
+        self, telegram_id: int, ids: List[PhotoEntity]
+    ) -> List[PhotoEntity]:
         raise NotImplementedError
 
     @abstractmethod
-    async def delete(self, telegram_id: int) -> List[PhotoUrlEntity] | None:
+    async def delete(self, telegram_id: int) -> List[PhotoEntity] | None:
         raise NotImplementedError
