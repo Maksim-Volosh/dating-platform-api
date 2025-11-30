a12dcdc07d461b966ada99a8ddb09faf74c04fad
Maksim Volosh <maksgoza9@gmail.com>
2025-11-30 16:08:14 +0200

    feat: rewrite DI containers

--- app/core/containers/photo.py
+++ app/core/containers/photo.py
@@ -6,8 +6,7 @@ from app.domain.use_cases import (DeleteUserPhotosUseCase,
                                   UpdateUserPhotosUseCase,
                                   UploadUserPhotosUseCase)
 from app.infrastructure.db import db_helper
-from app.infrastructure.repositories import (LocalPhotoStorage,
-                                             SQLAlchemyPhotoRepository)
+from app.infrastructure.repositories import SQLAlchemyPhotoRepository
 
 
 async def get_retrieve_user_photos_use_case(
@@ -23,27 +22,22 @@ async def get_upload_user_photos_use_case(
 ) -> UploadUserPhotosUseCase:
     photo_repo = SQLAlchemyPhotoRepository(db)
     return UploadUserPhotosUseCase(
-        photo_repo=photo_repo,
-        file_storage=file_storage
+        photo_repo=photo_repo
     )
     
 async def get_delete_user_photos_use_case(
     db: AsyncSession = Depends(db_helper.session_getter)
 ) -> DeleteUserPhotosUseCase:
     photo_repo = SQLAlchemyPhotoRepository(db)
-    file_storage = LocalPhotoStorage()
     return DeleteUserPhotosUseCase(
-        photo_repo=photo_repo,
-        file_storage=file_storage
+        photo_repo=photo_repo
     )
     
 async def get_update_user_photos_use_case(
     db: AsyncSession = Depends(db_helper.session_getter)
 ) -> UpdateUserPhotosUseCase:
     photo_repo = SQLAlchemyPhotoRepository(db)
-    file_storage = LocalPhotoStorage()
     return UpdateUserPhotosUseCase(
-        photo_repo=photo_repo,
-        file_storage=file_storage
+        photo_repo=photo_repo
     )
 
