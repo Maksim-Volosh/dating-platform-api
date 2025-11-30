40dc1172f0cda71fdfc821f5bc01e40954639730
Maksim Volosh <maksgoza9@gmail.com>
2025-11-30 15:46:49 +0200

    feat: change db structure

--- app/infrastructure/models/photos.py
+++ app/infrastructure/models/photos.py
@@ -1,4 +1,4 @@
-from sqlalchemy import UUID, ForeignKey, String
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
@@ -8,7 +8,7 @@ class Photo(Base):
     __tablename__ = "photo"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    url: Mapped[str] = mapped_column(String(255))
+    file_id: Mapped[str] = mapped_column(String(255))
 
     user_id: Mapped[int] = mapped_column(ForeignKey("user.telegram_id"))
     user: Mapped["User"] = relationship("User", back_populates="photos") # type: ignore
