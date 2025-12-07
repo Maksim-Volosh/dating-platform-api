59ecbb8afe12cf135d7a634c009fd97f8434fb05
Maksim Volosh <maksgoza9@gmail.com>
2025-12-07 09:18:43 +0200

    refactor: change structure

--- bot/app/services/__init__.py
+++ bot/app/services/__init__.py
@@ -1,9 +1,9 @@
-from .create_user import create_user_profile
-from .create_photos import create_photos_for_user
-from .get_photos import get_user_photos
-from .update_user import update_user_profile
-from .update_photos import update_photos_for_user
-from .update_description import update_description
+from .user.create_user import create_user_profile
+from .photos.create_photos import create_photos_for_user
+from .photos.get_photos import get_user_photos
+from .user.update_user import update_user_profile
+from .photos.update_photos import update_photos_for_user
+from .user.update_description import update_description
 
 __all__ = (
     "create_user_profile",
