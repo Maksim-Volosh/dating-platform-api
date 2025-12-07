582af6591d42d176ff35f7211a80cc134e99eae5
Maksim Volosh <maksgoza9@gmail.com>
2025-12-07 10:20:44 +0200

    fix: alter user_id column to bigint

--- app/infrastructure/models/swipe.py
+++ app/infrastructure/models/swipe.py
@@ -10,7 +10,7 @@ class Swipe(Base):
     __tablename__ = "swipe"
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    user1_id: Mapped[int]
+    user1_id: Mapped[int] = mapped_column(BigInteger)
     user1_decision: Mapped[Optional[bool]] = mapped_column(nullable=True)
-    user2_id: Mapped[int]
+    user2_id: Mapped[int] = mapped_column(BigInteger)
     user2_decision: Mapped[Optional[bool]] = mapped_column(nullable=True)
