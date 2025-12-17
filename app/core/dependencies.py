409541c393678f5531d3ffbe4bbb2bea455fde38
Maksim Volosh <maksgoza9@gmail.com>
2025-12-17 11:50:40 +0200

    fix: fix critical logical bug with comparing api_key

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -8,7 +8,7 @@ from app.domain.exceptions import UserNotFoundById
 
 
 async def verify_bot_key(x_api_key: str = Header(...)):
-    if x_api_key not in settings.security.api_key:
+    if x_api_key != settings.security.api_key:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid Client API Key")
