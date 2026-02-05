2ed39b070d2d3788b4bca87b4406631c3eaa29d6
Maksim Volosh <maksgoza9@gmail.com>
2026-02-05 22:31:49 +0200

    feat: create ai profile analizer service to analize user profile

--- app/application/services/__init__.py
+++ app/application/services/__init__.py
@@ -3,8 +3,10 @@ __all__ = (
     "InboxOnSwipeService",
     "GeoCandidateFilterService",
     "SwipeFilterService",
+    "AIProfileAnalizeService",
 )
 from .deck import DeckBuilderService
 from .inbox import InboxOnSwipeService
 from .geo_filter import GeoCandidateFilterService
 from .swipe_filter import SwipeFilterService
+from .ai_profile_analizer import AIProfileAnalizeService
---
+++ app/application/services/ai_profile_analizer.py
@@ -0,0 +1,55 @@
+from app.domain.entities import UserEntity
+from app.domain.exceptions import UserNotFoundById
+from app.domain.interfaces import IAIClientRepository, IUserRepository
+
+
+class AIProfileAnalizeService:
+    def __init__(
+        self,
+        ai_repo: IAIClientRepository,
+        user_repo: IUserRepository
+    ):
+        self.ai_repo = ai_repo
+        self.user_repo = user_repo
+        
+    def _format_message_by_user(self, user: UserEntity):
+        message = f"""
+            Ты анализируешь анкету пользователя в дейтинг-приложении.
+
+            Задача:
+            Кратко оценить анкету и дать обратную связь.
+            
+            Оценивай 
+
+            Формат ответа:
+            Плюсы:
+            1) ...
+            2) ...
+            3) ...
+
+            Минусы:
+            1) ...
+            2) ...
+
+            Ограничения:
+            - РОВНО 3 плюса и РОВНО 2 минуса (если плюсов нет, не указывай, не притягивай за уши, плюс ради плюса)
+            - ВЕСЬ ответ — не более 2 предложений
+            - Общий объём — не более ~600 символов
+            - С эмодзи
+            - Без морализаторства и советов «как жить»
+            - Пиши дружелюбно обращаясь по имены, конструктивно и по делу
+
+            Анкета пользователя:
+            Имя: {user.name}
+            Описание: {user.description}
+        """
+        return message
+    
+    async def analize(self, telegram_id: int):
+        user = await self.user_repo.get_by_id(telegram_id)
+        if user is None:
+            raise UserNotFoundById()
+        
+        message = self._format_message_by_user(user)
+        
+        return await self.ai_repo.complete(message)
